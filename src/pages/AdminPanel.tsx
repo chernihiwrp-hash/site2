8884b8b3c0a28f6fc87ffa987bd4aedebc5d55d9
chernihiwrp-hash <chernihiwrp@gmail.com>
2026-03-28 21:45:10 +0200

    Update AdminPanel.tsx

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -2751,7 +2751,7 @@ const BansTab = () => {
                     <Clock className="w-3 h-3 text-orange-400" />
                     <span className="text-[9px] text-orange-400 font-semibold">{timeLeft(ban.expires_at)}</span>
                   </div>
-               )}
+                  )}
               </div>
             </div>
           ))}
